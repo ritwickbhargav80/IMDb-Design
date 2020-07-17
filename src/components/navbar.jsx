0c5e15d998d5494feaf6104f6fafa96decce90d1
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-07-18 03:04:41 +0530

    --update: Navbar updated!

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../logo.svg";
+import "../stylesheets/navbar.css";
 
 const Navbar = () => {
   return (
@@ -21,18 +22,30 @@ const Navbar = () => {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="myid">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item pad-left active">
             <Link className="nav-link" to="/movies">
               Movies
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item pad-left">
             <Link className="nav-link" to="/shows">
               Tv Shows
             </Link>
           </li>
+        </ul>
+        <ul className="navbar-nav">
           <li className="nav-item">
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </form>
+          </li>
+          <li className="nav-item pad-left">
             <Link className="nav-link" to="/signin">
               Sign In
             </Link>
