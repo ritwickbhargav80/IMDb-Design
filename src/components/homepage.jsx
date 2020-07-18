77aeebfa0ad1b4f87083d9c991aa6b9c2407287a
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-07-19 02:40:29 +0530

    --update: Homepage updated!

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "../stylesheets/homepage.css";
 
 const HomePage = () => {
-  return <h1>Homepage</h1>;
+  let date = new Date();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "March",
+    "May",
+    "June",
+    "July",
+    "August",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = months[date.getMonth() + 1];
+  date = date.getDate();
+
+  return (
+    <div className="container">
+      <div>
+        <h3 className="h3">What to Watch</h3>
+        <div className="left-border">
+          <h5 className="sub-heading">Fan Favorites</h5>
+        </div>
+        <p className="sub-script">This week's top TV and movies</p>
+      </div>
+      <div>
+        <h3 className="h3">Explore what's streaming</h3>
+      </div>
+      <div>
+        <h3 className="h3">Today's Exclusive</h3>
+        <div className="left-border">
+          <h5 className="sub-heading">Top Coverage &gt;</h5>
+        </div>
+        <p className="sub-script">
+          People born on {month} {date}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
