ed6f744c220d82882f2e8b6a436297bcb3567e7a
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-08-04 23:32:21 +0530

    --try: trying react-slick

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Slider from "react-slick";
 
 import { getDateFunction } from "./../utils/common";
 
@@ -7,6 +8,39 @@ import "../stylesheets/homepage.css";
 const HomePage = () => {
   const { date, month } = getDateFunction();
 
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="container">
       <h3 className="h3">What to Watch</h3>
@@ -14,6 +48,15 @@ const HomePage = () => {
         <h5 className="sub-heading">Fan Favorites</h5>
       </div>
       <p className="sub-script">This week's top TV and movies</p>
+      <div className="container">
+        <div>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+          </Slider>
+        </div>
+      </div>
       <h3 className="h3">Explore what's streaming</h3>
       <h3 className="h3">Today's Exclusive</h3>
       <div className="left-border">
