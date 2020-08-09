3c4a565ffc5dfae02708fc9557a8fe5d8b45720f
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-08-09 22:28:14 +0530

    :zap: --update: slide.jsx updated!

--- src/components/common/slide.jsx
+++ src/components/common/slide.jsx
@@ -3,10 +3,14 @@ import React, { Component } from "react";
 import "../../stylesheets/slide.css";
 
 class Slide extends Component {
-  state = { show: false };
+  state = { show: false, showBtn: false };
 
   setIsShown(value) {
     this.setState({ show: value });
+  }
+
+  setBtnIsShown(value) {
+    this.setState({ showBtn: value });
   }
 
   render() {
@@ -27,26 +31,25 @@ class Slide extends Component {
               className="slide-text"
               style={{ opacity: this.state.show ? 1 : 0 }}
             >
-              <span class="content-title"> Lootcase</span>
-            </div>
-            <div class="content-info">
-              <div class="duration">2 hr 11 min</div>,{" "}
-              <span class="genre">Comedy, Hindi, 2020</span>
-            </div>
-            <div class="content-desc">
-              What will you do if you find a bag full of cash? Nandan Kumar
-              faces a similar dilemma when a bag tri ...
-            </div>
-            <div class="trailer-action">
-              <div class="trailer-icon"></div>
-              <div class="trailer">Watch trailer</div>
-            </div>
-            <div class="watchlist-action">
-              <div class="add-to-watchlist" title="Add to Watchlist">
-                <div class="watchlist-wrapper">
-                  <div class="watchlist-icon"></div>
-                  <p class="watchlist">Add to Watchlist</p>
-                </div>
+              <span className="content-title"> Lootcase</span>
+              <div className="content-info">
+                <div className="duration">2 hr 11 min</div>{" "}
+                <span className="genre">Comedy, Hindi, 2020</span>
+              </div>
+              <div className="content-desc">
+                What will you do if you find a bag full of cash? Nandan Kumar
+                faces a similar dilemma when a bag tri ...
+              </div>
+              <div className="add-to-watchlist" title="Add to Watchlist">
+                <p
+                  className="watchlist btn"
+                  onMouseEnter={() => this.setBtnIsShown(true)}
+                  onMouseLeave={() => this.setBtnIsShown(false)}
+                  style={{ opacity: this.state.showBtn ? 1 : 0.8 }}
+                >
+                  <i className="fa-plus plus-icon" aria-hidden="true" />
+                  <span className="btn-txt">Add to Watchlist</span>
+                </p>
               </div>
             </div>
           </div>
