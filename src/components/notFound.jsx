db6c89f5cec0c4a734172f08faec99734f10f92c
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-08-15 04:30:59 +0530

    :tada: 404-page done!

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,29 +1,65 @@
-import React from "react";
-
+import React, { Component } from "react";
 import image from "../assets/404.png";
+let array = require("../utils/data.json");
 
-const NotFound = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md">
-          <p className="content-404">
-            4
-            <span role="img" aria-label="0">
-              &#128549;
-            </span>
-            4 - Page Not Found
-          </p>
-          <div className="box sb4">
-            <span className="curly-quotes">&#8220; </span>
-            <span className="quotes">I'm speech bubble</span>
-            <span className="curly-quotes"> &#8221;</span>
+class NotFound extends Component {
+  state = {
+    randomQuote: {
+      quote: "",
+      movie: "",
+      year: 0,
+    },
+  };
+
+  componentDidMount() {
+    let randNum = Math.floor(Math.random() * array.length);
+    const { quote, movie, year } = array[randNum];
+    this.setState({ randomQuote: { quote, movie, year } });
+  }
+
+  render() {
+    let { quote, movie, year } = this.state.randomQuote;
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md text-center">
+            <p className="content-404">
+              4
+              <span role="img" aria-label="0">
+                &#128549;
+              </span>
+              4 - Page Not Found
+            </p>
+            <p className="desc-404">
+              The page you are looking for could not be found! Perhaps the page
+              moved, the URL is incorrect or maybe, just maybe, you tried to
+              travel to forbidden lands.
+            </p>
+            <div className="box sb4 row">
+              <span className="curly-quotes col-sm-1">&#8220; </span>
+              <span className="quotes col-sm-10">{quote}</span>
+              <span className="curly-quotes col-sm-1"> &#8221;</span>
+            </div>
+            <div className="quote-footer">
+              <p className="movie">{movie}</p>
+              <p className="year">{year}</p>
+            </div>
+            <input
+              className="btn create-acc-btn back-button"
+              type="submit"
+              value="Back to Homepage"
+            />
           </div>
+          <img
+            className="col-md img-container"
+            src={image}
+            alt="404 - Page Not Found"
+          />
         </div>
-        <img className="col-md" src={image} alt="404 - Page Not Found" />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default NotFound;
