87a71034bc57c9f65048dfdd2a0397043c9a4698
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-09-14 23:25:46 +0530

    --update: pagination done!

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -5,6 +5,14 @@ class Pagination extends Component {
     pageNo: 1,
   };
 
+  getArray = () => {
+    let { pageNo } = this.state;
+
+    if (pageNo <= 4) return [1, 2, 3, 4, 5];
+    else if (pageNo >= 497) return [496, 497, 498, 499, 500];
+    else return [pageNo - 2, pageNo - 1, pageNo, pageNo + 1, pageNo + 2];
+  };
+
   arrowStyle = (no) => {
     let { pageNo } = this.state;
 
@@ -24,6 +32,7 @@ class Pagination extends Component {
 
   render() {
     let { pageNo } = this.state;
+    const arr = this.getArray();
 
     return (
       <div className="row" style={{ color: "white" }}>
@@ -36,7 +45,7 @@ class Pagination extends Component {
           <div className="row" style={{ display: "inline-flex" }}>
             {pageNo <= 4 ? (
               <React.Fragment>
-                {[1, 2, 3, 4, 5].map((a) => (
+                {arr.map((a) => (
                   <p className="paginate" key={a}>
                     {a}
                   </p>
@@ -45,8 +54,30 @@ class Pagination extends Component {
             ) : (
               <p className="paginate">1</p>
             )}
-            <p className="paginate-dot">...</p>
-            <p className="paginate">500</p>
+            {pageNo > 4 && pageNo < 497 ? (
+              <React.Fragment>
+                <p className="paginate-dot">...</p>
+                {arr.map((a) => (
+                  <p className="paginate" key={a}>
+                    {a}
+                  </p>
+                ))}
+                <p className="paginate-dot">...</p>
+              </React.Fragment>
+            ) : (
+              <p className="paginate-dot">...</p>
+            )}
+            {pageNo >= 497 ? (
+              <React.Fragment>
+                {arr.map((a) => (
+                  <p className="paginate" key={a}>
+                    {a}
+                  </p>
+                ))}
+              </React.Fragment>
+            ) : (
+              <p className="paginate">500</p>
+            )}
           </div>
         </div>
         <i
